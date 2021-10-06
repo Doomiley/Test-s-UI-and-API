91c5038270ee2fe5ea5816fb2aa9002be0f51b23
doomiley@gmail.com <Ant213456>
2021-10-06 21:04:54 +0300

    Update Test API

--- src/test/java/models/TestApi.java
+++ src/test/java/models/TestApi.java
@@ -1,37 +1,48 @@
 package models;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class TestApi {
 
-    String getUrl = "https://api.github.com";
-    String basePath = "/users/doomiley";
+    private static RequestSpecification spec;
 
-    @Test
-    public void testUserInfoResponse() {
-        given()
-                .baseUri(getUrl)
-                .basePath(basePath)
-                .contentType(ContentType.JSON)
-                .when().get()
-                .then()
-                .statusCode(200)
-                .body("login", equalTo("Doomiley"));
+
+    @BeforeClass
+    public static void initSpec() {
+        spec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri("https://reqres.in/api")
+                .setBasePath("/users")
+                .build();
     }
 
-    @Test
-    public void testUserInfoResponseType() {
-        given()
-                .baseUri(getUrl)
-                .basePath(basePath)
-                .contentType(ContentType.JSON)
-                .when().get()
-                .then()
-                .statusCode(200)
-                .body("type", equalTo("User"));
-    }
+        @Test
+        public void testUserInfoResponseType() {
+         List<User> users = given()
+                 .spec(spec)
+                 .when()
+                 .get()
+                 .then()
+                 .statusCode(200)
+                 .extract().jsonPath().getList("data", User.class);
+
+         assertThat(users).extracting(User::getEmail).contains("george.bluth@reqres.in");
+        }
+
 }
+
+
+
